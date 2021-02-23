b9e52cdbc14b7197b16ab65a36bcad7351e3d8be
vakasgnabi <vakasghulamnabi88@gmail.com>
2021-02-23 10:13:50 +0500

    create product comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 // feature 1
 import React from "react";
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All rights are reserved</footer>
-    </div>
-  );
+import Products from "./components/Products";
+import data from "./data.json";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights are reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
